a8d8b0b635f967a7279b624c9729c5a868395426
Tyler Muth <tylerdmuth-git@yahoo.com>
2014-05-15 09:24:08 -0400

    Changed driver.sql to query-capture.sql

--- source/run-one-px-by-2.sh
+++ source/run-one-px-by-2.sh
@@ -13,5 +13,5 @@ for f in $FILES
 do
   echo "Processing $f file..."
   queryname=`echo "$f" | sed 's/\..\{3\}$//' | sed 's/[\/|\.]//g'`
-  \sqlplus d56/d56 @driver $f $queryname  null.sql
+  \sqlplus d56/d56 @query-capture.sql $f $queryname  null.sql
 done
