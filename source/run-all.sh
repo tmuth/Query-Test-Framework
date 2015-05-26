428f78ecb990979300027caae8294275c3779dd8
tmuth <tylerdmuth-git@yahoo.com>
2015-05-26 15:51:00 -0400

    Changed to array
    Using an arrary for test names, alter session files now instead of
    multiple files.

--- source/run-all.sh
+++ source/run-all.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-DIRECTORY=retail-queries2
-#OUTPUT_SUB_DIRECTORY="Test1"
-OUTPUT_SUB_DIRECTORY=`echo "d7.test_"$(date +"%m-%d_%H-%M-%S")`
-FILES=rtl*.sql
-ALTER_SESSION_FILE=no-in-memory.sql
-CONNECT_STRING=d7/d7
+
+## Edit variables in the following block ################################################################
+
+DIRECTORY=star-schema1
+MAIN_TEST_NAME="olap.500m.capacity"
+FILES=\*.sql
+CONNECT_STRING=olap_perf/olap_perf
 # . ~/./orcl.env
 
+
+declare -A testName # this is an associative array of testname:alter-session-file
+#testName[a]=null.sql
+testName[IMC-autovector]=in-memory-auto-vector.sql
+testName[IMC-novector]=in-memory-no-vector.sql
+testName[IMC-forcevector]=in-memory-vector.sql
+
+testName[NOIMC-autovector]=no-in-memory-auto-vector.sql
+testName[NOIMC-novector]=no-in-memory-novector.sql
+testName[NOIMC-forcevector]=no-in-memory-vector.sql
+
+## Do Not Edit Below This Line ###########################################################################
+
+
+
+
+ALTER_SESSION_SUBDIR=`echo "alter"`
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $SCRIPT_DIR
@@ -22,41 +40,80 @@ echo "$QUERY_CAPTURE_FILE"
 
 cd $DIRECTORY
 
+#for t in "${!testName[@]}"; do
+#    #printf "%s is in %s\n" "$t" "${testName[$t]}"
+#	OUTPUT_SUB_DIRECTORY=`echo "$MAIN_TEST_NAME.$t.test_"$(date +"%m-%d_%H-%M-%S")`
+#	#echo $OUTPUT_SUB_DIRECTORY
+#	ALTER_SESSION_FILE=${testName[$t]}
+#	
+#	ALTER_SESSION_FILE=`echo $ALTER_SESSION_SUBDIR/${testName[$t]}`
+#	#echo $ALTER_SESSION_FILE
+#	#cat $ALTER_SESSION_FILE
+#done
+
+tLength=${#testName[@]}
+declare -i loopCounter
+loopCounter=0
 
 
-if [[ ! -f ${f} ]];
-then
-	echo "ALTER_SESSION_FILE does not exist so creating a blank file for it"
-	touch ${ALTER_SESSION_FILE}
-fi
-
-if [[ ! -f ${OUTPUT_SUB_DIRECTORY} ]];
-then
-	echo "OUTPUT_SUB_DIRECTORY does not exist so creating it now"
-	mkdir ${OUTPUT_SUB_DIRECTORY}
-fi
-
-for f in ${FILES};
-do
-	if [ -f ${f} ]
+# main test loop
+for t in "${!testName[@]}"; do #loop over all of the tests in the test array
+	((loopCounter++))
+	echo "$(date +"%m-%d_%H:%M:%S") - Begin Test $loopCounter of $tLength \n" >> status.log
+    #printf "%s is in %s\n" "$t" "${testName[$t]}"
+	OUTPUT_SUB_DIRECTORY=`echo "$MAIN_TEST_NAME.$t.test_"$(date +"%m-%d_%H-%M-%S")`
+	echo $OUTPUT_SUB_DIRECTORY
+	ALTER_SESSION_FILE=`echo "$ALTER_SESSION_SUBDIR/${testName[$t]}"`
+	echo $ALTER_SESSION_FILE
+	
+	
+	if [[ ! -d ${ALTER_SESSION_SUBDIR} ]];
 	then
-		if [ `basename $ALTER_SESSION_FILE` != `basename $f` ]
-		then
-			echo "Processing $f file..."
-			queryname=`basename $f .sql | sed 's/\..\{3\}$//' | sed 's/[\/|\.]//g'`
-			echo "Query Name: $queryname"
-			
-			\sqlplus $CONNECT_STRING @"$QUERY_CAPTURE_FILE" $f $queryname "$ALTER_SESSION_FILE" $OUTPUT_SUB_DIRECTORY
-			errorCode=$?
-                        echo "Error Code: $errorCode"
-
-                        if [ "$errorCode" -gt 0 ]
-                        then
-                           break  # Skip entire rest of loop.
-                        fi
-
-		fi
+		echo "ALTER_SESSION_SUBDIR does not exist so creating it now"
+		mkdir ${ALTER_SESSION_SUBDIR}
 	fi
+	
+	if [[ ! -f ${ALTER_SESSION_FILE} ]];
+	then
+		echo "ALTER_SESSION_FILE does not exist so creating a blank file for it"
+		touch ${ALTER_SESSION_FILE}
+	fi
+
+	if [[ ! -d ${OUTPUT_SUB_DIRECTORY} ]];
+	then
+		echo "OUTPUT_SUB_DIRECTORY does not exist so creating it now"
+		mkdir ${OUTPUT_SUB_DIRECTORY}
+	fi
+		
+	for f in ${FILES}; # for each test, loop over all of the query files
+	do
+		if [ -f ${f} ]
+		then
+			if [ `basename $ALTER_SESSION_FILE` != `basename $f` ]
+			then
+				echo "Processing $f file..."
+				#queryname=`basename $f .sql | sed 's/\..\{3\}$//' | sed 's/[\/|\.]//g'`
+				queryname=`basename $f .sql | sed 's/\..\{3\}$//' | sed 's/[\/]//g'`
+				echo "Query Name: $queryname"
+				
+				\sqlplus $CONNECT_STRING @"$QUERY_CAPTURE_FILE" $f $queryname "$ALTER_SESSION_FILE" $OUTPUT_SUB_DIRECTORY
+				errorCode=$?
+							echo "Error Code: $errorCode"
+
+							if [ "$errorCode" -gt 0 ]
+							then
+							   break  # Skip entire rest of loop.
+							fi
+
+			fi
+		fi
+	done
+	
+	if [ "$errorCode" -gt 0 ]
+	then
+	   break  # Skip entire rest of loop.
+	fi
+	echo "$(date +"%m-%d_%H:%M:%S") - End   Test $loopCounter of $tLength \n" >> status.log
 done
 
-grep -m 1 Elapsed *.txt > timing.out
+echo "\n\n\n" >> status.log
